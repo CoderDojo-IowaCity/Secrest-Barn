ddba4566c3557020b35325ccc30d68adfe88800b
Dave Welch <david.m.welch@gmail.com>
2012-09-02 20:48:45 -0500

    Initial commit of basic elements for an HTML5 website

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/*
+This is a dummy Javascript file for the Secrest Barn website.  It should demonstrate all the basic Javascript functions
+as a primer.  For instance, this is an example of a MULTILINE COMMENT.  Everything between the "forward slash + star"
+and "star + forward slash" will not be executed by the computer.
+*/
+
+// This is a single line comment
+
+var testMessage = "Hello, Coders!"
